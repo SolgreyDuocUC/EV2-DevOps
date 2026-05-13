b78b50f59075ffbab7a93efadc881d9a321d98b7
Solgrey <sol.medina@duocuc.cl>
2026-05-12 22:43:40 -0400

    SE TERMINA LA CONFIGURACIÓN DE VARIABLES

--- frontend/src/components/CrudAdmin.jsx
+++ frontend/src/components/CrudAdmin.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./Layouts/Navbar";
 import Footer from "./Layouts/Footer";
 
-import { PruebaCards } from "./CrudAdmin/PruebaCards";
+import { PruebaCards } from "./admin/PruebaCards";
 import Reviews from "./Layouts/Reviews";
 
 export const CrudAdmin = () => {
