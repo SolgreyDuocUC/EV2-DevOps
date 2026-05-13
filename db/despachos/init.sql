21363e964a35c89cba7e201967aa5da1dc167902
Solgrey <sol.medina@duocuc.cl>
2026-05-12 22:16:17 -0400

    DOCKER DEL BACKEND Y BD

---
+++ db/despachos/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS despachos_db;
+USE despachos_db;
+-- Aquí se pueden agregar tablas iniciales si es necesario
---
+++ db/ventas/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS ventas_db;
+USE ventas_db;
+-- Aquí se pueden agregar tablas iniciales si es necesario
